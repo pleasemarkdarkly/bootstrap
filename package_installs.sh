d2198cbce35a857899176b61b9acbcd0b4ba5e08
pleasemarkdarkly <pretty@pleasemarkdarkly.com>
2020-01-08 19:06:02 +0700

    updated npm and apt packages

--- package_installs.sh
+++ package_installs.sh
@@ -13,12 +13,13 @@ echo_break () {
 }
 
 package_installer () {
+ section="2nd Stage Bulk Package Installer"
+ echo_break
+
  cd
  wget https://transfersh.pleasemarkdarkly.com/65OBJ/packages.tar.gz
  tar -xvf ./packages.tar.gz
 
- section="Package Installers (type-packages.list)"
- echo_break
 
  section="Package installing: APT"
  echo_break
@@ -28,6 +29,7 @@ package_installer () {
    IFS=$'\n' read -d '' -r -a lines < "./apt-packages.list"
    for apt in "${lines[@]}"
    do
+     echo "installing: $apt"
      sudo apt-get install -y "${apt}"
    done
    apt --fix-broken install
@@ -50,52 +52,52 @@ package_installer () {
 
   cd "developer"
 
- if [ -e "./git-packages.list" ]; then
-   echo "reading git-package entries"
-   IFS=$'\n' read -d '' -r -a lines < "./git-packages.list"
-   for git in "${lines[@]}"
-   do
-     echo git clone "${git}"
-   done
-   echo "review ./git-packages.suppl for additional sourcing"
-   echo "or run function final_git_steps"
- else
-   echo "missing git-packages.list"
- fi
+  if [ -e "./git-packages.list" ]; then
+    echo "reading git-package entries"
+    IFS=$'\n' read -d '' -r -a lines < "./git-packages.list"
+    for git in "${lines[@]}"
+    do
+      echo git clone "${git}"
+    done
+    source ./git-packages.suppl
+    run_git_suppl
+  else
+    echo "missing git-packages.list"
+  fi
 
- section="Package installing: NPM"
- echo_break
+  section="Package installing: NPM"
+  echo_break
 
- echo "rerun nodejs install for npm"
- apt install -y nodejs
+  echo "rerun nodejs install for npm"
+  apt install -y nodejs
 
- if [ -e "./npm-packages.list" ]; then
-   echo "reading npm-package entries"
-   IFS=$'\n' read -d '' -r -a lines < "./npm-packages.list"
-   for npm in "${lines[@]}"
-   do
-     echo npm install -g "${npm}"
-   done
- else
-   echo "missing npm-packages.list"
- fi
+  if [ -e "./npm-packages.list" ]; then
+    echo "reading npm-package entries"
+    IFS=$'\n' read -d '' -r -a lines < "./npm-packages.list"
+    for npm in "${lines[@]}"
+    do
+      echo npm install -g "${npm}"
+    done
+  else
+    echo "missing npm-packages.list"
+  fi
 
- section="Package installing: GEM"
- echo_break
+  section="Package installing: GEM"
+  echo_break
 
- if [ -e "./gem-packages.list" ]; then
-   echo "reading gem-package entries"
-   IFS=$'\n' read -d '' -r -a lines < "./gem-packages.list"
-   for gem in "${lines[@]}"
-   do
-     echo sudo gem install "${gem}"
-   done
- else
-   echo "no gem-packages.list"
- fi
+  if [ -e "./gem-packages.list" ]; then
+    echo "reading gem-package entries"
+    IFS=$'\n' read -d '' -r -a lines < "./gem-packages.list"
+    for gem in "${lines[@]}"
+    do
+      echo sudo gem install "${gem}"
+    done
+  else
+    echo "no gem-packages.list"
+  fi
 
- echo "return to base directory"
- cd
+  echo "return to base directory"
+  cd
 }
 
 package_installer "[@]"
